9dfd367a68e3c4be40a7cead654dcf75d314cc86
Alex <alexhcurtin@gmail.com>
2019-10-13 14:12:59 -0400

    add api call

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
+const API_KEY =
+  "ZAT7lUGBZGPLIkglRLWpBf-vVhqK2M2cRuZRqB0txpmrNLkRTH-heU6y1N0WjkvHwldkRtv6KR1kMoOS3QqZpV_umraVLyY2M-zfFqvV2rDFTQWajoQmcc7PFU6jXXYx";
+
+const options = {
+  headers: {
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+export const getParks = async location => {
+  const res = await axios.get(
+    `${BASE_URL}?categories=parks&location=${location}`,
+    options
+  );
+  console.log(res);
+  return res.data.businesses;
+};
